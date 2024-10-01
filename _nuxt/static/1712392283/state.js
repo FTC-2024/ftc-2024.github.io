7065e1da4125287c7846a897688229d5abd4568f
FTC-2024 <FTC-2024@users.noreply.github.com>
2024-10-01 14:42:07 +0000

    deploy: 5963772feba80cec21ae147be1de128641bee441

--- _nuxt/static/1712392283/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1712392283",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"fa029207"}}}}(null,"\u002F"));
---
+++ _nuxt/static/1727793723/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1727793723",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"798c1635"}}}}(null,"\u002F"));
